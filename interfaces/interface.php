e4a8d5b1cd2429967334df13f8e686ab343937ab
Pasan Gamage <pasan.gamage@previousnext.com.au>
2019-04-03 15:25:40 +1100

    [#master] Add an interface and make use of it

---
+++ interfaces/interface.php
@@ -0,0 +1,14 @@
+<?php
+
+interface TableInterface {
+  public function save(array $data);
+}
+
+class Table implements TableInterface {
+
+  public function save(array $data) {
+    echo 'foo';
+  }
+}
+
+echo (new Table())->save([]);
